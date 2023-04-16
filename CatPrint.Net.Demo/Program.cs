0235c818ceb48e11723bf3ab4e3efaf249786c95
Roman Buldygin <froooks@gmail.com>
2023-04-17 02:17:16 +0300

    Add project files.

---
+++ CatPrint.Net.Demo/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InTheHand.Bluetooth;
+
+Console.WriteLine("Hello, World!");
+
+var isavailable = await Bluetooth.GetAvailabilityAsync();
