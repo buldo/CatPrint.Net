93f375cdd6bb4f8321e1990d5f285ec4406d6c13
Roman Buldygin <froooks@gmail.com>
2023-04-17 02:42:26 +0300

    save

--- CatPrint.Net.Demo/Program.cs
+++ CatPrint.Net.Demo/Program.cs
@@ -2,5 +2,9 @@
 using InTheHand.Bluetooth;
 
 Console.WriteLine("Hello, World!");
-
-var isavailable = await Bluetooth.GetAvailabilityAsync();
+var discoveredDevices = await Bluetooth.ScanForDevicesAsync();
+foreach (var device in discoveredDevices)
+{
+    Console.WriteLine($"{device.Id} --- {device.Name} ---");
+}
+Console.WriteLine($"found {discoveredDevices?.Count} devices");
