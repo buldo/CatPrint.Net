e69c4d8f82673e7772a9b7f79537b1c86a7cbd63
Roman Buldygin <froooks@gmail.com>
2023-04-19 01:42:02 +0300

    It works!!!

--- CatPrint.Net.Demo/Program.cs
+++ CatPrint.Net.Demo/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using CatPrint.Net;
 using InTheHand.Bluetooth;
 
 Console.WriteLine("Hello, World!");
@@ -15,27 +17,63 @@ if (device == null)
     return;
 }
 
-if (!device.Gatt.IsConnected)
-{
-    await device.Gatt.ConnectAsync();
-}
+var printer = new Printer(device);
+await printer.InitAsync();
 
-var services = await device.Gatt.GetPrimaryServicesAsync();
-
-var serviceUuid = BluetoothUuid.FromGuid(Guid.Parse("0000ae30-0000-1000-8000-00805f9b34fb"));
-var txCharacteristicUuid = BluetoothUuid.FromGuid(Guid.Parse("0000ae01-0000-1000-8000-00805f9b34fb"));
-var rxCharacteristicUuid = BluetoothUuid.FromGuid(Guid.Parse("0000ae02-0000-1000-8000-00805f9b34fb"));
-
-var service = services.FirstOrDefault(srv => srv.Uuid == serviceUuid);
-if (service == null)
-{
-    Console.WriteLine("No expected Service");
-    return;
-}
-
-var txCharacteristic = await service.GetCharacteristicAsync(txCharacteristicUuid);
-var rxCharacteristic = await service.GetCharacteristicAsync(rxCharacteristicUuid);
-
+var commandsFactory = new CommandsFactory();
+await printer.SendAsync(commandsFactory.CreateFeedPaper(80));
 
 Console.WriteLine("Ready for work");
 Console.ReadLine();
+
+
+public class Printer
+{
+    private const int MTU = 248;
+    private readonly BluetoothDevice _device;
+    private GattCharacteristic _txCharacteristic;
+
+    public Printer(BluetoothDevice device)
+    {
+        _device = device;
+    }
+
+    public async Task InitAsync()
+    {
+        if (!_device.Gatt.IsConnected)
+        {
+            await _device.Gatt.ConnectAsync();
+        }
+
+        var services = await _device.Gatt.GetPrimaryServicesAsync();
+
+        var serviceUuid = BluetoothUuid.FromGuid(Guid.Parse("0000ae30-0000-1000-8000-00805f9b34fb"));
+        var txCharacteristicUuid = BluetoothUuid.FromGuid(Guid.Parse("0000ae01-0000-1000-8000-00805f9b34fb"));
+        var rxCharacteristicUuid = BluetoothUuid.FromGuid(Guid.Parse("0000ae02-0000-1000-8000-00805f9b34fb"));
+
+        var service = services.FirstOrDefault(srv => srv.Uuid == serviceUuid);
+        if (service == null)
+        {
+            Console.WriteLine("No expected Service");
+            return;
+        }
+
+        _txCharacteristic = await service.GetCharacteristicAsync(txCharacteristicUuid);
+        var rxCharacteristic = await service.GetCharacteristicAsync(rxCharacteristicUuid);
+    }
+
+    public async Task SendAsync(Command command)
+    {
+        await SendBytes(command.AsBytes());
+    }
+
+    private async Task SendBytes(byte[] command)
+    {
+        // 4 bytes required for L2CAP header
+        var chunks = command.Chunk(MTU - 4);
+        foreach (var chunk in chunks)
+        {
+            await _txCharacteristic.WriteValueWithoutResponseAsync(chunk);
+        }
+    }
+}
---
+++ CatPrint.Net/CommandsFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatPrint.Net;
+
+public class CommandsFactory
+{
+    public Command CreateFeedPaper(UInt16 steps)
+    {
+        var buffer = new byte[2];
+        BinaryPrimitives.WriteUInt16LittleEndian(buffer, steps);
+        return new(0xA1, buffer);
+    }
+
+}
